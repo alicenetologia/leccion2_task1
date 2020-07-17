cd49b1e36f213541c7ab021dda9b07bc856e49b1
Olesia D <osd0581@gmail.com>
2020-07-17 14:11:59 +0200

    Implement Money Transfer

---
+++ src/Money.java
@@ -0,0 +1,11 @@
+public class Money {
+
+    public static void main(String[] args) {
+        int current  = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = current + transfer;
+        System.out.println("Current amount: " + current);
+        System.out.println("Transfer : " + transfer);
+        System.out.println("Total amount: " + total);
+    }
+}
